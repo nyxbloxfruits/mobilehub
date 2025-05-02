c34337966f3d38efaa891b168385895762e2833c
nyxbloxfruits <nyx.blox.fruits@gmail.com>
2025-05-02 06:08:44 -0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,11 +1,15 @@
 repeat wait() until game:IsLoaded()
-if game.PlaceId ~= 2753915549 and game.PlaceId ~= 4442272183 and game.PlaceId ~= 7449423635 then return end
 
-_G.MobileHub_Config = {
-    JoinTeam = "Pirates",
+if game.PlaceId ~= 2753915549 and game.PlaceId ~= 4442272183 and game.PlaceId ~= 7449423635 then
+    return
+end
+
+getgenv().MobileHub_Config = {
+    JoinTeam = "Pirates", -- Pirates ou Marines
     Translator = true,
-    Owner = "nyxbloxfruits",
-    HubName = "MobileHub"
+    HubName = "MobileHub",
+    Creator = "nyxbloxfruits"
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/main/Source.luau"))(_G.MobileHub_Config)
+-- Menu baseado no estilo Redz, mas com nome e autoria personalizados
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nyxbloxfruits/MobileHub/main/Core/MobileUI.lua"))()
