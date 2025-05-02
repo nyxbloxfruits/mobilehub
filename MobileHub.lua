8480cf61ff686100e217add587d713a745b7dbbd
nyxbloxfruits <nyx.blox.fruits@gmail.com>
2025-05-02 04:04:15 -0300

    Create MobileHub.lua
    Estilo redz

---
+++ MobileHub.lua
@@ -0,0 +1,6 @@
+local Settings = {
+    JoinTeam = "Pirates", -- Pirates or Marines
+    Translator = true -- Traduzir menu para PT-BR
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/main/Source.luau"))(Settings)
